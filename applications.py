6db208d961abed609619142fe132df96d64ceb56
Oscar941 <128169229+Oscar941@users.noreply.github.com>
2024-06-06 12:42:15 +0300

    optimization

--- applications.py
+++ applications.py
@@ -6,42 +6,47 @@ load_dotenv()
 
 DATABASE_PATH = os.getenv('DATABASE_PATH', 'job_applications.db')
 
-def create_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect(DATABASE_PATH)
-    except sqlite3.Error as e:
-        print(e)
-    return conn
+class DBConnection:
+    def __init__(self, db_path):
+        self.db_path = db_path
+        self.conn = None
+
+    def __enter__(self):
+        try:
+            self.conn = sqlite3.connect(self.db_path)
+            return self.conn
+        except sqlite3.Error as e:
+            print(e)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.conn:
+            self.conn.close()
 
 def add_application(application_details):
-    conn = create_connection()
-    sql = '''INSERT INTO applications(company_name, position, date_applied, status, notes)
-             VALUES(?,?,?,?,?)'''
-    cur = conn.cursor()
-    cur.execute(sql, (application_details['company_name'], application_details['position'],
-                      application_details['date_applied'], application_details['status'],
-                      application_details['notes']))
-    conn.commit()
-    conn.close()
+    with DBConnection(DATABASE_PATH) as conn:
+        sql = '''INSERT INTO applications(company_name, position, date_applied, status, notes)
+                 VALUES(?,?,?,?,?)'''
+        cur = conn.cursor()
+        cur.execute(sql, (application_details['company_name'], application_details['position'],
+                          application_details['date_applied'], application_details['status'],
+                          application_details['notes']))
+        conn.commit()
 
 def get_applications():
-    conn = create_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM applications")
-    applications = cur.fetchall()
-    conn.close()
-    return applications
+    with DBConnection(DATABASE_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM applications")
+        applications = cur.fetchall()
+        return applications
 
 def update_application(app_id, updates):
-    conn = create_connection()
-    sql = f"UPDATE applications SET "
-    sql += ', '.join([f"{column} = ?" for column in updates.keys()])
-    sql += " WHERE id = ?"
-    cur = conn.cursor()
-    cur.execute(sql, list(updates.values()) + [app_id])
-    conn.commit()
-    conn.close()
+    with DBConnection(DATABASE_PATH) as conn:
+        sql = f"UPDATE applications SET "
+        sql += ', '.join([f"{column} = ?" for column in updates.keys()])
+        sql += " WHERE id = ?"
+        cur = conn.cursor()
+        cur.execute(sql, list(updates.values()) + [app_id])
+        conn.commit()
 
 def change_status(app_id, new_status):
-    update_application(app_id, {'status': new_status})
+    update_application(app_id, {'status': new_definitions})
