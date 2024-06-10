2abd1730ad0231f23a3fa0e0b9573296c10dae3b
Oscar941 <128169229+Oscar941@users.noreply.github.com>
2024-06-10 16:50:21 +0300

    add new variables

--- applications.py
+++ applications.py
@@ -6,56 +6,56 @@ load_dotenv()
 
 DATABASE_PATH = os.getenv('DATABASE_PATH', 'job_applications.db')
 
-class DBConnection:
-    def __init__(self, db_path):
-        self.db_path = db_path
-        self.conn = None
+class DatabaseConnector:
+    def __init__(self, database_path):
+        self.database_path = database_path
+        self.connection = None
 
     def __enter__(self):
         try:
-            self.conn = sqlite3.connect(self.db_path)
-            return self.conn
-        except sqlite3.Error as e:
-            print(e)
+            self.connection = sqlite3.connect(self.database_path)
+            return self.connection
+        except sqlite3.Error as error:
+            print(error)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.conn:
-            self.conn.close()
+        if self.connection:
+            self.connection.close()
 
-def add_application(application_details):
-    with DBConnection(DATABASE_PATH) as conn:
-        sql = '''INSERT INTO applications(company_name, position, date_applied, status, notes)
-                 VALUES(?,?,?,?,?)'''
+def insert_application(application_data):
+    with DatabaseConnector(DATABASE_PATH) as connection:
+        insert_query = '''INSERT INTO applications(company_name, position, date_applied, status, notes)
+                          VALUES(?,?,?,?,?)'''
         try:
-            cur = conn.cursor()
-            cur.execute(sql, (application_details['company_name'], application_details['position'],
-                              application_details['date_applied'], application_details['status'],
-                              application_details['notes']))
-            conn.commit()
+            cursor = connection.cursor()
+            cursor.execute(insert_query, (application_data['company_name'], application_data['position'],
+                                          application_data['date_applied'], application_data['status'],
+                                          application_data['notes']))
+            connection.commit()
         finally:
-            cur.close()
+            cursor.close()
 
-def get_applications():
-    with DBConnection(DATABASE_PATH) as conn:
+def fetch_applications():
+    with DatabaseConnector(DATABASE_PATH) as connection:
         try:
-            cur = conn.cursor()
-            cur.execute("SELECT * FROM applications")
-            applications = cur.fetchall()
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM applications")
+            applications = cursor.fetchall()
             return applications
         finally:
-            cur.close()
+            cursor.close()
 
-def update_application(app_id, updates):
-    with DBConnection(DATABASE_PATH) as conn:
-        sql = "UPDATE applications SET "
-        sql += ', '.join([f"{column} = ?" for column in updates.keys()])
-        sql += " WHERE id = ?"
+def modify_application(application_id, modifications):
+    with DatabaseConnector(DATABASE_PATH) as connection:
+        update_query = "UPDATE applications SET "
+        update_query += ', '.join([f"{field} = ?" for field in modifications.keys()])
+        update_nguery += " WHERE id = ?"
         try:
-            cur = conn.cursor()
-            cur.execute(sql, list(updates.values()) + [app_id])
-            conn.commit()
+            cursor = connection.cursor()
+            cursor.execute(update_query, list(modifications.values()) + [application_id])
+            connection.commit()
         finally:
-            cur.close()
+            cursor.close()
 
-def change_status(app_id, new_status):
-    update_application(app_id, {'status': new_status})
+def update_application_status(application_id, status_update):
+    modify_application(application_id, {'status': status_update})
